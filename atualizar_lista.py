bdce52453d1b663314165100476e45fb19807754
althierestm <althierestm@gmail.com>
2026-07-26 12:27:35 -0300

    Update atualizar_lista.py

--- atualizar_lista.py
+++ atualizar_lista.py
@@ -3,20 +3,22 @@ import os
 # Configurações da sua Emissora (Radar TV)
 PLAYLIST_NAME = "Radar TV"
 LOGO_URL = "https://raw.githubusercontent.com/althierestm/radartv/main/Logo%20RadarTV%203000x3000.jpg"
+EPG_URL = "https://raw.githubusercontent.com/althierestm/radartv/main/epg.xml"
 
 # Canal FCV TV (Sinal principal)
 CANAL_PRINCIPAL_FCV = "https://video01.logicahost.com.br/fcvtv/fcvtv/playlist.m3u8"
 
 
 def create_m3u():
-    m3u_content = '#EXTM3U\n'
+    # Adicionamos o link do EPG direto na tag #EXTM3U
+    m3u_content = f'#EXTM3U x-tvg-url="{EPG_URL}"\n'
     m3u_content += f'#EXTINF:-1 tvg-id="RadarTV" tvg-name="{PLAYLIST_NAME}" tvg-logo="{LOGO_URL}" group-title="Radar TV", {PLAYLIST_NAME}\n'
     m3u_content += f'{CANAL_PRINCIPAL_FCV}\n'
 
     with open("lista.m3u", "w", encoding="utf-8") as f:
         f.write(m3u_content)
         
-    print("✅ Arquivo lista.m3u atualizado com o sinal da FCV TV!")
+    print("✅ Arquivo lista.m3u gerado com suporte a EPG!")
 
 
 if __name__ == "__main__":
