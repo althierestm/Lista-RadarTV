852c7279112743532f01db429fddaff720b317a1
althierestm <althierestm@gmail.com>
2026-07-26 14:40:13 -0300

    Update atualizar_lista.py

--- atualizar_lista.py
+++ atualizar_lista.py
@@ -10,15 +10,15 @@ CANAL_PRINCIPAL_FCV = "https://video01.logicahost.com.br/fcvtv/fcvtv/playlist.m3
 
 
 def create_m3u():
-    # Adicionamos o link do EPG direto na tag #EXTM3U
-    m3u_content = f'#EXTM3U x-tvg-url="{EPG_URL}"\n'
+    # Adicionamos todas as variações de tags EPG para funcionar em Smart TVs e SS IPTV
+    m3u_content = f'#EXTM3U url-tvg="{EPG_URL}" x-tvg-url="{EPG_URL}"\n'
     m3u_content += f'#EXTINF:-1 tvg-id="RadarTV" tvg-name="{PLAYLIST_NAME}" tvg-logo="{LOGO_URL}" group-title="Radar TV", {PLAYLIST_NAME}\n'
     m3u_content += f'{CANAL_PRINCIPAL_FCV}\n'
 
     with open("lista.m3u", "w", encoding="utf-8") as f:
         f.write(m3u_content)
         
-    print("✅ Arquivo lista.m3u gerado com suporte a EPG!")
+    print("✅ Arquivo lista.m3u gerado com suporte total a EPG!")
 
 
 if __name__ == "__main__":
